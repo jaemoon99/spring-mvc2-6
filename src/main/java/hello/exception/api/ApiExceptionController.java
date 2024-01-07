460c581c5e018e1ad62c1be79d51403067eeba93
jaemoon <woans5970@naver.com>
2024-01-07 20:57:19 +0900

    section 9: HandlerExceptionResolver

--- src/main/java/hello/exception/api/ApiExceptionController.java
+++ src/main/java/hello/exception/api/ApiExceptionController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ApiExceptionController {
 
-    @GetMapping("/api/members/{id}")
+//    @GetMapping("/api/members/{id}")
     public MemberDto getMember(@PathVariable("id") String id) {
 
         if (id.equals("ex")) {
@@ -20,6 +20,17 @@ public class ApiExceptionController {
 
         return new MemberDto(id, "hello " + id);
     }
+
+    @GetMapping("/api/members/{id}")
+    public MemberDto getMemberV2(@PathVariable("id") String id) {
+        if (id.equals("ex")) {
+            throw new RuntimeException("잘못된 사용자");
+        }
+        if (id.equals("bad")) {
+            throw new IllegalArgumentException("잘못된 입력 값"); }
+        return new MemberDto(id, "hello " + id);
+    }
+
 
     @Data
     @AllArgsConstructor
